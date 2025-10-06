bc34689ec9f9e08b17428be0084312d98dd90620
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-10-06 14:39:00 -0400

    Add howler plugin

---
+++ plugins/howler/constants.py
@@ -0,0 +1,5 @@
+import os
+
+APP_NAME = os.environ.get("APP_NAME", "howler-dev")
+CLASSIFICATION = os.environ.get("CLASSIFICATION", "TLP:CLEAR")
+HOWLER_URL = os.environ.get("HOWLER_URL", "https://howler.dev.analysis.cyber.gc.ca")
