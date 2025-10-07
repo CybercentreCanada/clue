2d9bb1077d0fa30728e7c70196413c10b9378b30
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-10-07 09:17:03 -0400

    Reworked conftest

--- plugins/conftest.py
+++ plugins/conftest.py
@@ -1,13 +1,27 @@
 import sys
 from pathlib import Path
 
-folder = Path(sys.argv[1]).resolve()
+import pytest
 
-sys.path.insert(0, str(folder))
 
-try:
-    import clue  # noqa: F401
-except ImportError:
-    clue_folder = (Path(__file__).parent.parent / "api").resolve()
-    sys.path.append(str(clue_folder))
-    sys.path.append(str(clue_folder / f".venv/lib/python3.{sys.version_info.minor}/site-packages"))
+@pytest.fixture(scope="session", autouse=True)
+def inject_paths(pytestconfig):
+    """Injects specified paths into sys.path for pytest session.
+
+    Parameters
+    ----------
+    pytestconfig : _pytest.config.Config
+        The pytest configuration object containing command-line arguments.
+    """
+    for _path in pytestconfig.args:
+        folder = Path(_path).resolve()
+
+        if folder.exists():
+            sys.path.insert(0, str(folder))
+
+    try:
+        import clue  # noqa: F401
+    except ImportError:
+        clue_folder = (Path(__file__).parent.parent / "api").resolve()
+        sys.path.append(str(clue_folder))
+        sys.path.append(str(clue_folder / f".venv/lib/python3.{sys.version_info.minor}/site-packages"))
