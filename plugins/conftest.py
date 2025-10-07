102a717b2bf7986bca13afbb19c767511207ef24
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-10-07 08:55:19 -0400

    Adding venv to tests

--- plugins/conftest.py
+++ plugins/conftest.py
@@ -8,5 +8,6 @@ sys.path.insert(0, str(folder))
 try:
     import clue  # noqa: F401
 except ImportError:
-    clue_folder = Path(__file__).parent.parent / "api"
-    sys.path.insert(1, str(clue_folder))
+    clue_folder = (Path(__file__).parent.parent / "api").resolve()
+    sys.path.append(str(clue_folder))
+    sys.path.append(str(clue_folder / f".venv/lib/python3.{sys.version_info.minor}/site-packages"))
