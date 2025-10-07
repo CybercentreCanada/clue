9f5ab3632eb465e0c5875c0317467ee93f0c4d7a
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-10-07 08:49:53 -0400

    More unit testing

--- plugins/conftest.py
+++ plugins/conftest.py
@@ -4,3 +4,9 @@ from pathlib import Path
 folder = Path(sys.argv[1]).resolve()
 
 sys.path.insert(0, str(folder))
+
+try:
+    import clue  # noqa: F401
+except ImportError:
+    clue_folder = Path(__file__).parent.parent / "api"
+    sys.path.insert(1, str(clue_folder))
