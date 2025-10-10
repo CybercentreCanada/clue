b0193cc278a8d46d7ae7a939392f5a762abc4007
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-10-10 12:07:54 -0400

    Added test dotenv for howler plugin

--- plugins/howler/constants.py
+++ plugins/howler/constants.py
@@ -1,4 +1,11 @@
 import os
+import sys
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+dotenv_path = None if "pytest" not in sys.modules else Path(__file__).parent / "test" / ".env.test"
+load_dotenv(dotenv_path=dotenv_path)
 
 APP_NAME = os.environ.get("APP_NAME", "howler-dev")
 CLASSIFICATION = os.environ.get("CLASSIFICATION", "TLP:CLEAR")
