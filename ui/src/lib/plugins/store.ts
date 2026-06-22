e506f8415a39b424fc9c36f089deccc9f3c91f14
cccs-RyanK <ryan.kane@cyber.gc.ca>
2026-06-22 10:40:10 -0400

    fix get plugin logic

--- ui/src/lib/plugins/store.ts
+++ ui/src/lib/plugins/store.ts
@@ -80,13 +80,21 @@ export class ClueUIPluginStore {
    */
   getPlugins(format?: string, resultType?: 'action' | 'fetcher', actionId?: string, fetcherId?: string): string[] {
     let pluginsById: string[] | undefined = undefined;
+    let pluginsWithId: Set<string> | undefined = undefined;
     let pluginsByFormat: string[] = [];
     let pluginsByResultType: string[] | undefined = undefined;
 
+    if (!format && !resultType && !actionId && !fetcherId) {
+      // if no criteria is given return all plugins
+      return this.plugins;
+    }
+
     if (actionId) {
       pluginsById = this.pluginsByActionId[actionId];
+      pluginsWithId = new Set(Object.values(this.pluginsByActionId).flat());
     } else if (fetcherId) {
       pluginsById = this.pluginsByFetcherId[fetcherId];
+      pluginsWithId = new Set(Object.values(this.pluginsByFetcherId).flat());
     }
 
     pluginsByResultType =
@@ -98,20 +106,27 @@ export class ClueUIPluginStore {
       return [];
     }
 
-    const availablePlugins =
-      pluginsById?.length || pluginsByResultType?.length
-        ? [
-            ...(pluginsById?.length
-              ? pluginsByFormat.filter(plugin => {
-                  return pluginsById.includes(plugin);
-                })
-              : []),
-            ...(pluginsByResultType?.length
-              ? pluginsByFormat.filter(plugin => pluginsByResultType.includes(plugin))
-              : [])
-          ]
-        : pluginsByFormat;
+    let availablePlugins: string[] = [];
+    // first get plugins that specify this action/fetcher Id
+    if ((actionId || fetcherId) && pluginsById?.length) {
+      availablePlugins = pluginsByFormat.filter(plugin => {
+        return pluginsById.includes(plugin);
+      });
+    }
 
+    // get fallback plugins that match the resultType and format, but do not specify an action/fetcher Id
+    if (resultType) {
+      availablePlugins = [
+        ...availablePlugins,
+        ...(pluginsByResultType?.length
+          ? (pluginsByFormat.filter(plugin => pluginsByResultType.includes(plugin) && !pluginsWithId.has(plugin)) ?? [])
+          : [])
+      ];
+    } else if (format) {
+      // if there is no result type given, return the plugins that match the format
+      availablePlugins = [...availablePlugins, ...pluginsByFormat.filter(plugin => !pluginsWithId.has(plugin))];
+    }
+    // return unique available plugins (only first unique occurance is kept)
     return uniq(availablePlugins);
   }
 
